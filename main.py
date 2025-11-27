8a225c75fe9937185881122e8ddae0a210515c1c
Teodor Jireada <teodor.jireada@student.tuiasi.ro>
2025-11-27 13:11:35 +0200

    raport intermediar

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
 import os
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from ui.main_window import MainWindow
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setApplicationName("DAW proiect PIU")
+    app.setWindowIcon(QIcon('assets/logo.png'))
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
